86228e2e07e6b5b05fb1237296f156edcab24118
ppopiolek <papopiolek@gmail.com>
2024-03-14 15:08:15 +0100

    established cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prractial Adversarial Attacks on Flow-Based Network Intrusion Detection Systems',
+    author='Paweł Popiołek',
+    license='',
+)
